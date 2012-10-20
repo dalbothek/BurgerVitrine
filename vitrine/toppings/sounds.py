b7335e71ec4f178715cfcc8d43507fd99325c5b0
Simon Marti <simon@ceilingcat.ch>
2012-10-20 02:20:43 +0200

    Added sounds topping

---
+++ vitrine/toppings/sounds.py
@@ -0,0 +1,20 @@
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+
+from .topping import Topping
+
+
+class SoundsTopping(Topping):
+    KEY = "sounds"
+    NAME = "Sounds"
+    ITEMS = (
+        ("name", "Name"),
+        ("package", "Package")
+    )
+    PRIORITY = 3.5
+
+    def parse_entry(self, entry, key=None):
+        return entry["name"]
